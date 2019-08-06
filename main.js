fa7b1e19c976eb80a6ff0b63199db091b2c01b58
Aron Winston <aronwinston2792@gmail.com>
2019-08-06 14:06:53 -0400

    added javascript scrolling effect

--- main.js
+++ main.js
@@ -1,26 +1,45 @@
-$(function() {
-    //Code along for image gallery
-    
-    let gallery = $(".project").find("img");
-    gallery.css("width", "22%").css("opacity", "0.7");
-    
-    gallery.mouseenter(function(){
-      $(this).stop().fadeTo(500, 1)
-    })
-    
-    gallery.mouseleave(function(){
-      $(this).stop().fadeTo(500, .7)
-    })
-    
-    gallery.click(function(){
-      let source = $(this).attr("src");
-      let image = $("<img>").attr("src", source).css("width", "100%");
-      $(".lightbox").empty().append(image).fadeIn(1000);
-    })
-    
-    $(".lightbox").click(function(){
-      $(this).stop().fadeOut(1000);
-    })
-    
-    
-    });
+'use strict'
+
+let one = document.querySelector("#one");
+let skills = document.querySelector(".skills");
+let two = document.querySelector("#two");
+let about = document.querySelector(".about");
+let three = document.querySelector("#three");
+let projects = document.querySelector(".projects");
+let four = document.querySelector("#four");
+let education = document.querySelector(".education");
+let five = document.querySelector("#five");
+let resume = document.querySelector(".resume");
+
+
+console.log(skills);
+console.log(about);
+
+
+window.addEventListener ('scroll', (event) => {
+  if (window.scrollY > 200){
+      skills.classList.add("fadein");
+      one.classList.add("fadein");
+  }
+
+  if(window.scrollY > 400){
+    about.classList.add("fadein");
+    two.classList.add("fadein");
+  }
+
+  if(window.scrollY > 800){
+    projects.classList.add("fadein");
+    three.classList.add("fadein");
+  }
+
+  if(window.scrollY > 1200){
+    education.classList.add("fadein");
+    four.classList.add("fadein");
+  }
+
+  if(window.scrollY > 1600){
+    resume.classList.add("fadein");
+    five.classList.add("fadein");
+  }
+});
+
